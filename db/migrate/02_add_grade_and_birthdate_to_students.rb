8b76524bb005505037f7d9ef501f331b7271325f
Joseph Sesti <sestijoey63@gmail.com>
2019-10-31 18:06:05 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,4 +2,7 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
     add_column :students do |s|
       s.integer :grade
-      
+      s.string :birthdate
+    end
+  end
+end
